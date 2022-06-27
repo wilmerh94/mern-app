7e04d32fa695ee015919c3bcdd03c6cffe2cb8e7
Wilmer <herrerawilmer.94@gmail.com>
2022-06-27 17:06:57 -0600

    backend completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,7 +70,12 @@ const loginUser = asyncHandler(async (req, res) => {
 // @access Private
 
 const getMe = asyncHandler(async (req, res) => {
- res.send('me');
+ const user = {
+  id: req.user._id,
+  email: req.user.email,
+  name: req.user.name,
+ };
+ res.status(200).json(user);
 });
 
 // Generate TOKEN
